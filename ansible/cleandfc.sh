5c47fe3adac14d073a975666fd229100984e41ec
Ubuntu <ubuntu@ip-10-0-0-72.us-east-2.compute.internal>
2018-05-15 19:05:41 +0000

    run cleandfcstate from nodes in the cluster
    run cleandfcstate on nodes and run it from cleandf.sh from controller node
    
    run cleandfcstate on nodes and run it from cleandf.sh from controller node
    
    fileformat=unix
    
    fileformat=unix
    
    setupnodes.sh remove sudo in dfc dir create and check if go is installed already
    
    Adding logic to add new targets and cleanup DFC cluster
    
    Adding logic to add new targets and cleanup DFC cluster
    
    clean new targets as well
    
    stopandcleandfc before cleaning data
    
    Fix looking for GET workload
    
    add target quickly to test
    
    fix aws_cluster.py params
    
    fix workload detection in runperftest
    
    minor fixes
    
    minor fixes
    
    minor fixes
    
    minor fixes
    
    minor fixes
    
    minor fixes
    
    minor fixes

--- ansible/cleandfc.sh
+++ ansible/cleandfc.sh
@@ -1,12 +1,8 @@
+#!/bin/bash
+set -o
+./stopandcleandfc.sh
 echo "Cleaning up targets"
-parallel-ssh -h inventory/targets.txt -i 'ls /dfc'
-parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /var/log/dfc*'
-parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /dfc/localbuckets'
-parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /dfc/mpaths'
-parallel-ssh -t 180 -h inventory/targets.txt -i 'sudo rm -rf /dfc/*/*'
-parallel-ssh -h inventory/targets.txt -i 'ls /dfc'
-parallel-ssh -h inventory/targets.txt -i 'df -h'
+parallel-ssh -h inventory/targets.txt -i './cleandfcstate.sh'
+parallel-ssh -h inventory/new_targets.txt -i './cleandfcstate.sh'
 echo "Cleaning up proxy"
-parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /var/log/dfc*'
-parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /dfc/*'
-parallel-ssh -h inventory/proxy.txt -i 'ls /dfc'
+parallel-ssh -h inventory/proxy.txt -i './cleandfcstate.sh'
--- ansible/cleandfcstate.sh
+++ ansible/cleandfcstate.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-set -o
-echo "Cleaning up targets"
-parallel-ssh -h inventory/targets.txt -i 'ls /dfc'
-parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /var/log/dfc*'
-parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /dfc/localbuckets'
-parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /dfc/mpaths'
-parallel-ssh -t 180 -h inventory/targets.txt -i 'sudo rm -rf /dfc/*/*'
-parallel-ssh -h inventory/targets.txt -i 'ls /dfc'
-parallel-ssh -h inventory/targets.txt -i 'df -h'
-echo "Cleaning up proxy"
-parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /var/log/dfc*'
-parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /dfc/*'
-parallel-ssh -h inventory/proxy.txt -i 'ls /dfc'
+ls /dfc
+sudo rm -rf /var/log/dfc*
+sudo rm -rf /dfc/localbuckets
+sudo rm -rf /dfc/mpaths
+sudo rm -rf /dfc/xvd*/*
+ls /dfc
+df -h
