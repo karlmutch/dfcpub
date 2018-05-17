146fbcd72d0486df3bffa96e36b458d59ce83795
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-05-17 19:57:13 +0000

    stopandcleandfc before cleaning data

--- ansible/cleandfc.sh
+++ ansible/cleandfc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -o
+./stopandcleandfc.sh
 echo "Cleaning up targets"
 parallel-ssh -h inventory/targets.txt -i './cleandfcstate.sh'
 parallel-ssh -h inventory/new_targets.txt -i './cleandfcstate.sh'
