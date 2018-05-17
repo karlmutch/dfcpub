c1c85d45318775fa97c13c739db6498fbe22479a
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-05-17 19:42:26 +0000

    Adding logic to add new targets and cleanup DFC cluster

--- ansible/runperftest.sh
+++ ansible/runperftest.sh
@@ -2,19 +2,37 @@
 set -o xtrace
 set -e
 
+FILE='inventory/new_targets.txt'
+WORKLOAD=$3
+echo Third argument: "$WORKLOAD"
+echo Remaining arguments: "$@"
+
 cmd="./startclient.sh "$@
 echo Running client cmd - $cmd
 parallel-ssh -t 0 -h inventory/clients.txt -i $cmd
 
 clients_running=`parallel-ssh -h inventory/clients.txt -i "screen -ls client" | grep client | wc -l`
 sleep 20
+count=0
 while [ $clients_running -gt 0 ]; do 
 	clients_running=`parallel-ssh -h inventory/clients.txt -i "screen -ls client" | grep client | wc -l` 
 	echo running 
 	parallel-ssh -h inventory/clients.txt -i 'tail -10 /home/ubuntu/dfc/src/github.com/NVIDIA/dfcpub/cmd/dfcloader/screenlog.0'
 	parallel-ssh -h inventory/targets.txt -i "iostat -xm 5 -c 2 | tail -33" || true
 	parallel-ssh -h inventory/targets.txt -i "netstat -s | grep transmit" || true
-	sleep 30 
+	sleep 30
+	count=$((count+1))
+	if [ $WORKLOAD == '-p 0' ]; then
+		if [ $count eq 10 ]; then
+			new_target=`$(head -n 1 $FILE)`
+			tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE" || true
+			echo Adding target $new_target
+			echo $new_target >> 'inventory/targets.txt'
+			ssh $new_target 'cat dfc.json'
+			ssh $new_target './starttarget.sh'
+			count=0
+		fi
+	fi
 done
 
 parallel-ssh -h inventory/clients.txt -i 'tail -20 /home/ubuntu/dfc/src/github.com/NVIDIA/dfcpub/cmd/dfcloader/screenlog.0'
