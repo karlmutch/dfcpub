56303524157b04efd8f48e1e40a4101694ee6d5e
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-05-17 19:55:06 +0000

    clean new targets as well

--- ansible/cleandfc.sh
+++ ansible/cleandfc.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 set -o
 echo "Cleaning up targets"
 parallel-ssh -h inventory/targets.txt -i './cleandfcstate.sh'
+parallel-ssh -h inventory/new_targets.txt -i './cleandfcstate.sh'
 echo "Cleaning up proxy"
 parallel-ssh -h inventory/proxy.txt -i './cleandfcstate.sh'
