bd05aac7e6e98cc8bc5264c9fef69ac8427effd8
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-05-18 04:50:44 +0000

    stopandclean newtargets as well

--- ansible/stopandcleandfc.sh
+++ ansible/stopandcleandfc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 parallel-ssh -h inventory/targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill || true'
 parallel-ssh -h inventory/targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill || true'
+parallel-ssh -h inventory/new_targets.txt -i 'ps -C dfc -o pid= | xargs sudo kill || true'
 parallel-ssh -h inventory/proxy.txt -i 'ps -C dfc -o pid= | xargs sudo kill'
 parallel-ssh -h inventory/proxy.txt -i 'sudo rm -rf /var/log/dfc*'
 parallel-ssh -h inventory/targets.txt -i 'sudo rm -rf /var/log/dfc*'
