d1267e0a200416a3ebde981b5601e2c06271ac68
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-06-01 15:36:43 +0000

    devtest.sh cd to DFCDIR

--- ansible/devtest.sh
+++ ansible/devtest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd $1/dfcpub/ansible
-python -u aws_cluster.py --command restart --cluster Cluster3
-echo running devtest on branch $BRANCH
 parallel-scp -h inventory/targets.txt rundevtest.sh '/home/ubuntu/'
 ssh $(head -1 inventory/targets.txt) './rundevtest.sh master'
 EXIT_STATUS=$?
