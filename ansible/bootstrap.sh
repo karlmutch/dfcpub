a851712619c4a31c747739c52dfd7f81cc363170
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-06-21 21:01:33 +0000

    add volume create call to bootstrap.sh

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+echo Creating disks total of $#
+
+sh ./createdfcvolumes.sh $#
+
 for disk in "$@"; do
     sudo mkdir -p /dfc/$disk || true
     sudo mkfs -t xfs /dev/$disk || true
