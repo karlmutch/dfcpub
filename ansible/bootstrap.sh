c868076edc9b6bdbe427d19a3a886512a2d585dd
Sachin Sanap <sachin.sanap.r@gmail.com>
2018-06-22 17:17:26 +0000

    Fix EBS volume creation and deletion for perf tests

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-set -e
+set -xo
+
 echo Creating disks total of $#
 
 chmod 744 createdfcvolumes.sh
 ./createdfcvolumes.sh $#
 
+echo Create FS dirs
 for disk in "$@"; do
-    sudo mkdir -p /dfc/$disk || true
-    sudo mkfs -t xfs /dev/$disk || true
+    parallel-ssh -h inventory/targets.txt -P 'sudo mkdir -p /dfc/'$disk
+    parallel-ssh -h inventory/new_targets.txt -P 'sudo mkdir -p /dfc/'$disk
 done
+
+echo Wait 30 sec before creating FS
+sleep 30
+
+echo Create FS
+for disk in "$@"; do
+    echo Create XFS on $disk
+    parallel-ssh -h inventory/targets.txt -P 'sudo mkfs -t xfs /dev/'$disk
+    parallel-ssh -h inventory/new_targets.txt -P 'sudo mkfs -t xfs /dev/'$disk
+done
+
--- ansible/createdfcvolumes.sh
+++ ansible/createdfcvolumes.sh
@@ -8,25 +8,29 @@ if [ -z "$volcount" ]; then
 fi
 
 echo '' > attachvolumes.sh
-echo '' > removevolumes.sh
+echo '' > detachvolumes.sh
+echo '' > deletevolumes.sh
+
 declare -a devices=(l m n o p q r s t u v w x y z)
-for i in `cat inventory/targets.txt`; do
+for i in `cat inventory/targets.txt; cat inventory/new_targets.txt`; do
         instanceId=$(aws ec2 describe-instances --filters "Name=private-ip-address,Values='$i'" | grep InstanceId | cut -d\" -f4)
         echo Target IP $i the instanceId is $instanceId
         for j in $(seq 1 $1); do
-                volumeId=$(aws ec2 create-volume --availability-zone us-east-2b --volume-type standard --size 1000 | grep VolumeId | cut -d\" -f4)
+                volumeId=$(aws ec2 create-volume --availability-zone us-east-2b --volume-type standard --size 100 | grep VolumeId | cut -d\" -f4)
                 echo New volume created $volumeId
                 device=/dev/xvd${devices[$((j-1))]}
                 echo aws ec2 attach-volume --volume-id ${volumeId} --instance-id ${instanceId} --device ${device} | tee -a attachvolumes.sh
-                echo aws ec2 detach-volume --volume-id ${volumeId} --force | tee -a removevolumes.sh
-                echo aws ec2 delete-volume --volume-id ${volumeId} | tee -a removevolumes.sh
+                echo aws ec2 detach-volume --volume-id ${volumeId} --force | tee -a detachvolumes.sh
+                echo aws ec2 delete-volume --volume-id ${volumeId} | tee -a deletevolumes.sh
         done
 done
 
 chmod 744 attachvolumes.sh
-chmod 744 removevolumes.sh
+chmod 744 detachvolumes.sh
+chmod 744 deletevolumes.sh
+
 echo Give some time for volume create to finish, sleep for 30 sec
 sleep 30
 echo Running attachvolumes.sh
 
-attachvolumes.sh
+./attachvolumes.sh
